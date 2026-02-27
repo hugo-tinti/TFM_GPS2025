b03fa9d1750ce5b0e6f73970c9bc5d0f20740e30
Hugo A. Tinti <HugoTinti@gmail.com>
2026-02-27 12:36:02 -0300

    version final render con Procfile

--- TFM_GPS2025.py
+++ TFM_GPS2025.py
@@ -1,3 +1,10 @@
+import os
+from groq import Groq
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+client = Groq(api_key=GROQ_API_KEY)
+
 from groq import Groq
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 #!/usr/bin/env python3
